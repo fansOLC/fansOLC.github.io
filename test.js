72125ed1ec474cdf2c775a877a1bcc7e0c722acd
fansOLC <894813150@qq.com>
2020-03-26 14:56:06 +0800

    【add dev content】

--- test.js
+++ test.js
@@ -2,9 +2,7 @@
  * @Author: fansolc
  * @Date: 2020-03-26 14:34:20
  * @LastEditors: fansolc
- * @LastEditTime: 2020-03-26 14:49:11
+ * @LastEditTime: 2020-03-26 14:55:52
  * @Description: test
  */
-let flag = '5'
-console.log('this is'+flag)
-console.log("add")
+console.log("这是dev内容")
