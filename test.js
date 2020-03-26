15c8f6b16d12618b3296964ffe88713c808c28f7
fansOLC <894813150@qq.com>
2020-03-26 14:45:54 +0800

    [add] 5

--- test.js
+++ test.js
@@ -2,8 +2,8 @@
  * @Author: fansolc
  * @Date: 2020-03-26 14:34:20
  * @LastEditors: fansolc
- * @LastEditTime: 2020-03-26 14:44:43
+ * @LastEditTime: 2020-03-26 14:45:42
  * @Description: test
  */
-let flag = '1'
+let flag = '5'
 console.log('this is'+flag)
