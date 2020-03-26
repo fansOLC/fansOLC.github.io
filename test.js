bc4b61d0ba3fd0f03288f0a5a795e9bb9226927b
fansOLC <894813150@qq.com>
2020-03-26 14:44:56 +0800

    [reverse]

--- test.js
+++ test.js
@@ -2,8 +2,8 @@
  * @Author: fansolc
  * @Date: 2020-03-26 14:34:20
  * @LastEditors: fansolc
- * @LastEditTime: 2020-03-26 14:40:25
+ * @LastEditTime: 2020-03-26 14:44:43
  * @Description: test
  */
-let flag = '3'
+let flag = '1'
 console.log('this is'+flag)
