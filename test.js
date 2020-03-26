e038691ddf4cfe15e1bf76fcf1fa1543f93d6e25
fansOLC <894813150@qq.com>
2020-03-26 14:49:22 +0800

    [凌轩]

--- test.js
+++ test.js
@@ -2,8 +2,9 @@
  * @Author: fansolc
  * @Date: 2020-03-26 14:34:20
  * @LastEditors: fansolc
- * @LastEditTime: 2020-03-26 14:35:15
+ * @LastEditTime: 2020-03-26 14:51:21
  * @Description: test
  */
 let flag = '1'
 console.log('this is'+flag)
+console.log("add")
