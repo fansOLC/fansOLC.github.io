a5abdecc9cf14a1289c31eb0aa96929b1bbfce1e
fansOLC <894813150@qq.com>
2020-03-26 14:40:44 +0800

    [update] three

--- test.js
+++ test.js
@@ -2,8 +2,8 @@
  * @Author: fansolc
  * @Date: 2020-03-26 14:34:20
  * @LastEditors: fansolc
- * @LastEditTime: 2020-03-26 14:37:29
+ * @LastEditTime: 2020-03-26 14:40:25
  * @Description: test
  */
-let flag = '2'
+let flag = '3'
 console.log('this is'+flag)
