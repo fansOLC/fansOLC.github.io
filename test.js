02d677f749bf98217f0a91a7cc62f8a9444a7775
fansOLC <894813150@qq.com>
2020-03-26 14:38:09 +0800

    [update] second

--- test.js
+++ test.js
@@ -2,8 +2,8 @@
  * @Author: fansolc
  * @Date: 2020-03-26 14:34:20
  * @LastEditors: fansolc
- * @LastEditTime: 2020-03-26 14:35:15
+ * @LastEditTime: 2020-03-26 14:37:29
  * @Description: test
  */
-let flag = '1'
+let flag = '2'
 console.log('this is'+flag)
